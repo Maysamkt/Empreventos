8c031813cbc7818bb29ffe69ad0a8d56e12c26d2
Maysamkt <maysamkt@hotmail.com>
2025-06-11 22:22:01 -0300

    feat: Entity User and Role

---
+++ src/main/java/br/edu/ifgoiano/Empreventos/model/Role.java
@@ -0,0 +1,92 @@
+package br.edu.ifgoiano.Empreventos.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "TINYINT")
+    private Byte id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true, length = 10)
+    private RoleName name;
+
+    @Column(length = 255)
+    private String description;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    // Enum para os valores fixos
+    public enum RoleName {
+        ADMIN, SPEAKER, LISTENER, ORGANIZER
+    }
+
+    public Role() {}
+    public Role(RoleName name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Byte getId() {
+        return id;
+    }
+
+    public void setId(Byte id) {
+        this.id = id;
+    }
+
+    public RoleName getName() {
+        return name;
+    }
+
+    public void setName(RoleName name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && name == role.name && Objects.equals(description, role.description) && Objects.equals(users, role.users);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, users);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name=" + name +
+                ", description='" + description + '\'' +
+                ", users=" + users +
+                '}';
+    }
+}
