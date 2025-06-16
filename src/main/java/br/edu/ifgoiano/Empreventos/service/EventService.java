00cdd1ccf465ec7b644ee225824a7ce3d880ef78
Marlon Neves <marlonmota2019@gmail.com>
2025-06-15 23:34:41 -0300

    at

--- src/main/java/br/edu/ifgoiano/Empreventos/service/EventService.java
+++ src/main/java/br/edu/ifgoiano/Empreventos/service/EventService.java
@@ -7,9 +7,13 @@ import br.edu.ifgoiano.Empreventos.mapper.DataMapper;
 import br.edu.ifgoiano.Empreventos.model.Activity;
 import br.edu.ifgoiano.Empreventos.model.ComplementaryMaterial;
 import br.edu.ifgoiano.Empreventos.model.Event;
+import br.edu.ifgoiano.Empreventos.model.User;
+import br.edu.ifgoiano.Empreventos.repository.UserRepository;
+import br.edu.ifgoiano.Empreventos.security.jwt.UserDetailsImpl;
 import br.edu.ifgoiano.Empreventos.util.EventStatus;
 import br.edu.ifgoiano.Empreventos.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -23,6 +27,9 @@ public class EventService {
 
     @Autowired
     private EventRepository eventRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
 
     private final Logger logger = Logger.getLogger(EventService.class.getName());
@@ -57,11 +64,18 @@ public class EventService {
 
     public EventDTO create(EventDTO eventDTO) {
         logger.info("Criando um Evento!");
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User organizer = userRepository.findById(userDetails.getId())
+                .orElseThrow(() -> new NoSuchElementException("Organizador não encontrado."));
+
         var eventEntity = DataMapper.parseObject(eventDTO, Event.class);
         eventEntity.setStatus(EventStatus.DRAFT);
+        eventEntity.setOrganizer(organizer);
         var savedEvent = eventRepository.save(eventEntity);
         return DataMapper.parseObject(savedEvent, EventDTO.class);
     }
+
 
     public EventDTO update(Long id, EventDTO eventDTO) {
         logger.info("Atualizando um Evento!");
